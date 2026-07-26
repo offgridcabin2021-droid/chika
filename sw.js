3129f44d9f972420e1cedc8779eb58e2ca3bd725
offgridcabin2021-droid <offgridcabin2021-droid@users.noreply.github.com>
2026-07-26 01:06:51 -0400

    feat: localize UI to Korean and introduce game mode tabs with monster catching functionality

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chika-timer-v13';
+const CACHE_NAME = 'chika-timer-v14';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
